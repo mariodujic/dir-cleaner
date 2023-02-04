cf303036b32b949805516bf1d8249b6fe0e540b6
Mario Dujić <mario.dujic@gmail.com>
2023-02-04 20:05:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Info
+
+Removes duplicate files across all sub-directories of a given folder.
+
+# Usage
+
+```
+git clone https://github.com/mariodujic/dir-cleaner
+cd dir-cleaner
+cargo install --path .
+cargo run <target_directory_path>
+```
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use std::{
+    env,
+    fmt::{Debug, Formatter, Result},
+    fs::remove_file,
+    ops::Add,
+};
+
+use dialoguer::Confirm;
+use std::cmp::Ordering;
+use walkdir::WalkDir;
+
+#[derive(Eq, PartialEq)]
+struct FileInfo {
+    name: String,
+    path: String,
+    size: u64,
+}
+
+impl PartialOrd for FileInfo {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for FileInfo {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let name_cmp = self.name.cmp(&other.name);
+        if name_cmp == Ordering::Equal {
+            self.size.cmp(&other.size)
+        } else {
+            name_cmp
+        }
+    }
+}
+
+impl Debug for FileInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        f.debug_struct("FileInfo")
+            .field("name", &self.name)
+            .field("path", &self.path)
+            .field("size", &self.size.to_string().add(" kb"))
+            .finish()
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = &args[1];
+
+    if Confirm::new()
+        .with_prompt("Do you want to continue?")
+        .interact()
+        .unwrap()
+    {
+        println!("Searching duplicate files..");
+        if clear_folder_duplicates(path) {
+            println!("Removed duplicate items.");
+        } else {
+            println!("No duplicate files found.");
+        }
+    } else {
+        println!("Nevermind then.");
+    }
+}
+
+fn clear_folder_duplicates(path: &String) -> bool {
+    let mut dir_files: Vec<FileInfo> = WalkDir::new(path)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .map(|entry| {
+            let file_name = entry.file_name().to_str().unwrap_or_default().to_string();
+            let path = entry.path().to_str().unwrap_or_default().to_string();
+            let file_size = entry.metadata().unwrap().len();
+            FileInfo {
+                name: file_name,
+                path: path,
+                size: file_size,
+            }
+        })
+        .collect();
+
+    dir_files.sort();
+
+    let max_index = dir_files.len() - 1;
+    let mut cleared_items = 0;
+    for (index, file) in dir_files.iter().enumerate() {
+        if index < max_index {
+            let next_file = dir_files.get(index + 1).unwrap();
+            if file.name == next_file.name && file.size == next_file.size {
+                println!("{:?}\n", &next_file);
+                cleared_items += 1;
+                remove_file(&next_file.path).unwrap();
+            }
+        }
+    }
+    cleared_items != 0
+}
